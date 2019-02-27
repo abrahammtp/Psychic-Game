a421f413a42607a09c8522d7072c88ce7cdb1320
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-02-27 18:59:14 -0500

    Started testing the choices

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,13 @@
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var wins = 0;
+
+var loses = 0;
+
+var guessesLeft = 0;
+
+var lettersUsed = 0;  
+
     var computerLetter = options[Math.floor(Math.random()*options.length)];
     console.log(computerLetter);
 
@@ -8,6 +16,9 @@ var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
     
 
    if (playerguess == computerLetter) {
-       alert("You chose one");
+       alert("Wins " + wins);
    }
-}
+    else if (playerguess !== computerLetter) {
+        alert("Loses " + loses);
+    }
+   }
