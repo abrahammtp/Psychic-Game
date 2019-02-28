33f4dfffa957d15c13531cb68b600ef1c9a6bbbc
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-02-28 15:34:06 -0500

    Finished working on the logic of the game, now it is time to add comments and work on how the website is going to look

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,10 @@ var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 // Variables that are going to be displayed on the website
 var wins = 0;
 
-var loses = 0;
+var losses = 0;
 
 var guessesLeft = 9; 
+document.getElementById("guesses-left").textContent = guessesLeft;
 
 var lettersUsed = []; 
 
@@ -33,18 +34,31 @@ var computerLetter = options[Math.floor(Math.random()*options.length)];
         alert("Try a different letter");
     }
 
-    else if (playerguess !== computerLetter && guessesLeft > 1) {
+    else if (playerguess !== computerLetter && guessesLeft > 0) {
         guessesLeft--;
         document.getElementById("guesses-left").textContent = guessesLeft;
         lettersUsed.push(playerguess); 
         document.getElementById("guesses").textContent = lettersUsed;
+    
     }
 
-    else if (playerguess == computerLetter && guessesLeft > 0) {
+    else if (playerguess == computerLetter) {
         wins++;
+        lettersUsed = [];
+        guessesLeft = 9;
         document.getElementById("wins").textContent = wins;
-        
-        
+        computerLetter = options[Math.floor(Math.random()*options.length)];
+        console.log(computerLetter);
+
+    }
+
+    if (guessesLeft == 0) {
+        losses++;
+        lettersUsed = [];
+        document.getElementById("guesses").textContent = lettersUsed;
+        document.getElementById("losses").textContent = losses;
+        computerLetter = options[Math.floor(Math.random()*options.length)];
+        console.log(computerLetter);
     }
 
 } 
